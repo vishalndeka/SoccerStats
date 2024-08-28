3f67d7b686539e49a0d3254dff6f67eccc2bf010
vishalndeka <72727433+vishalndeka@users.noreply.github.com>
2024-08-29 01:37:20 +0530

    Changed bb annotation to triangles and ellipse

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from inference import get_model
 import supervision as sv
 import cv2
+from tqdm import tqdm
 
 def install_dependencies():
     # works in a CUDA enabled GPU environment
@@ -10,6 +11,7 @@ def install_dependencies():
     os.system("pip install -q git+https://github.com/roboflow/supervision.git")
 
 def download_vids()->None:
+    # downloads a few sample videos
     if not os.path.exists(os.path.join(os.getcwd(), 'vids')):
         os.mkdir(os.path.join(os.getcwd(), 'vids'))
     os.chdir(os.path.join(os.getcwd(), 'vids'))
@@ -26,28 +28,67 @@ def get_roboflow_model():
     return PLAYER_DETECTION_MODEL
 
 def supervision_utilities():
-    box_annotator = sv.BoxAnnotator(color=sv.ColorPalette.from_hex(['#FF8C00', '#00BFFF', '#FF1493', '#FFD700']), thickness=2)
-    label_annotator = sv.LabelAnnotator(color=sv.ColorPalette.from_hex(['#FF8C00', '#00BFFF', '#FF1493', '#FFD700']), text_color=sv.Color.from_hex('#000000'))
-    return box_annotator, label_annotator
+    # colors for classes: ball, gk, outfield, ref
+    # box_annotator = sv.BoxAnnotator(color=sv.ColorPalette.from_hex(['#ffffff', '#00BFFF', '#FF1493', '#FFD700']), thickness=2)
+    # label_annotator = sv.LabelAnnotator(color=sv.ColorPalette.from_hex(['#ffffff', '#00BFFF', '#FF1493', '#FFD700']), text_color=sv.Color.from_hex('#000000'))
+    ellipse_annotator = sv.EllipseAnnotator(color=sv.Color.from_hex('#ffffff'), thickness=2)
+    triangle_annotator = sv.TriangleAnnotator(color=sv.ColorPalette.from_hex(['#ffffff', '#00BFFF', '#FF1493', '#FFD700']), base=20, height=17)
+    return ellipse_annotator, triangle_annotator
+
+def full_vid_detection(source_video_path) -> None:
+    SOURCE_VIDEO_PATH = source_video_path
+    TARGET_VIDEO_PATH = 'ops\\full_obj_detection.mp4'
+    BALL_ID = 0 # class id for ball class
+
+    # supervision function that can extract info from video
+    video_info = sv.VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
+    video_sink = sv.VideoSink(TARGET_VIDEO_PATH, video_info=video_info) # supervision utility that helps in saving video
+
+    with video_sink:
+        # frame generator for looping over frames
+        frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH, start=300)
+        # frame = next(frame_generator) # inference on the first frame
+
+        player_detection_model = get_roboflow_model()
+        # object detection on each frame
+        for frame in tqdm(frame_generator, total=video_info.total_frames):
+            result = player_detection_model.infer(frame, confidence=0.3)[0]
+            detections = sv.Detections.from_inference(result)
+
+            box_annotator, label_annotator = supervision_utilities()
+            labels = [f"{class_name} {confidence:.2f}" for class_name, confidence in zip(detections['class_name'], detections.confidence)]
+            annotated_frame = frame.copy()
+            annotated_frame = box_annotator.annotate(scene=annotated_frame, detections=detections)
+            annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections, labels=labels)
+            video_sink.write_frame(annotated_frame)
 
 def main():
+    # os.environ['ONNXRUNTIME_EXECUTION_PROVIDERS'] = "[CUDAExecutionProvider]"
     # download_vids() # done
-    os.environ['ONNXRUNTIME_EXECUTION_PROVIDERS'] = "[CUDAExecutionProvider]"
+    # full_vid_detection('vids\\full_obj_detection.mp4')
+    
+    # obj detection with triangle and ellipse annotators
     SOURCE_VIDEO_PATH = 'vids\\0bfacc_0.mp4'
-    # frame generator for looping over frames
-    frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH, start=300)
-    frame = next(frame_generator)
+    TARGET_VIDEO_PATH = 'ops\\0bfacc_0.mp4'
+    BALL_ID = 0 # class id for ball class
 
+    frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH, start = 300)
+    frame = next(frame_generator)
     player_detection_model = get_roboflow_model()
     result = player_detection_model.infer(frame, confidence=0.3)[0]
     detections = sv.Detections.from_inference(result)
-    labels = [f"{class_name} {confidence:.2f}" for class_name, confidence in zip(detections['class_name'], detections.confidence)]
-    box_annotator, label_annotator = supervision_utilities()
+    ball_detections = detections[detections.class_id == BALL_ID]
+    all_detections = detections[detections.class_id != BALL_ID]
+
+    ellipse_annotator, triangle_annotator = supervision_utilities()
     annotated_frame = frame.copy()
-    annotated_frame = box_annotator.annotate(scene=annotated_frame, detections=detections)
-    annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections, labels=labels)
+    annotated_frame = triangle_annotator.annotate(scene=annotated_frame, detections=all_detections)
+    annotated_frame = ellipse_annotator.annotate(scene=annotated_frame, detections=ball_detections)
+    
     sv.plot_image(annotated_frame)
-    cv2.imwrite("frame", frame)
+    cv2.imshow("frame", frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__=='__main__':
     main()
